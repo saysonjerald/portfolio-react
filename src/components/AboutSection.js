082b76ea6305ffd19dd0a2141dd17742373121fe
Jerald Sayson <saysonjerald@gmail.com>
2021-04-28 22:38:28 +0800

    12. Added FramerMotion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,25 +1,38 @@
 import React from 'react';
+import {motion} from 'framer-motion';
+
+//Image
 import home1 from '../img/home1.png';
 //Styled
 import {About, Description, Image, Hide } from '../styles';
+
+const titleAnim = {
+    hidden: {opacity: 0},
+    show: {opacity: 1, transition: {duration: 1}}
+}
+
+const container = {
+    hidden: {x: 100},
+    show: {x: 0, transition: {duration: 0.75, ease: "easeOut", staggerChildren: 1, when:'beforeChildren'}}
+}
 
 const AboutSection = () => {
     return(
         <About>
             <Description>
-                <div className="title">
+                <motion.div variants={container} initial="hidden" animate="show" className="title">
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2 variants={titleAnim} initial="hidden" animate="show" >We work to make</motion.h2 >
                     </Hide>
                     <Hide>
-                        <h2>
+                        <motion.h2 variants={titleAnim}>
                             your <span>dreams</span> come
-                        </h2>
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>Contract us for  any photography or videography ideas that you have. We have professionals with amazing skills.</p>
                 <button>Contacts Us</button>
             </Description>
