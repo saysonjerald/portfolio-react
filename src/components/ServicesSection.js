c285205c01f635c0b0bf00bffcb270c0011a7d29
Jerald Sayson <saysonjerald@gmail.com>
2021-04-30 11:01:04 +0800

    19. Scroll Animations

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import styled from 'styled-components'
+import { useScroll } from './useScroll';
+
 //Import Icons
 import clock from '../img/clock.svg';
 import diaphragm from '../img/diaphragm.svg';
 import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
 import home2 from '../img/home2.png';
+import {fade} from '../animation'
 
 //Styles
 import {About, Description, Image } from '../styles';
 
 
 const ServicesSection = () => {
+
+    const [element, controls] = useScroll();
+
     return (
-        <Services>
+        <Services variants={fade} animate={controls} initial="hidden" ref={element}>
             <Description>
                 <h2>High <span>quality</span> services</h2>
                 <Cards>
